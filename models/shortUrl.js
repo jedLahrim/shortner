624fc66a409c9cef932999f97d7a82d7ef5ad1f7
jed <jolix123@gmail.com>
2023-09-21 21:50:14 +0100

    commit 06

---
+++ models/shortUrl.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const shortId = require('shortid')
+
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  title: {
+    type: String,
+    required: false
+  },
+  image: {
+    type: String,
+    required: false
+  }
+})
+
+module.exports = mongoose.model('ShortUrl', shortUrlSchema)
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express()
+
+mongoose.connect('mongodb://dbJolixUserName:dbPassword@localhost:27017', {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(()=>{
+  console.log(`db connected successfully`)
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrl: null })
+})
+
+app.post('/shortUrl', async (req, res) => {
+ const shortUrl =  await ShortUrl.create({ full: req.body.fullUrl,
+    title:'hello from server',
+    image:'https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg' })
+  res.render('index', { shortUrl: shortUrl.short })
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+  shortUrl.title = `hello from server`
+  shortUrl.image = `https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg`
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+  console.log(`server running on port 3000`)
+});
