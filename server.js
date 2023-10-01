309e74e426feaa3e3c0713e96d5405784ee4042b
jed <jolix123@gmail.com>
2023-10-01 18:48:53 +0100

    commit 66

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-mongoose.connect('mongodb://dbJolixUserName:dbPassword@localhost:27017', {
+mongoose.connect('mongodb+srv://jolix1235:3ui5Bq6KlKvqol5U@cluster0.jxsmrah.mongodb.net/shortner', {
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=>{
   console.log(`db connected successfully`)
